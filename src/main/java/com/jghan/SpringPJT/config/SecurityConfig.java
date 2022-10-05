836ee776ba51ec43ec822b19dd6f2f625f6569c3
jghan <gojkhan13@gmail.com>
2022-10-05 18:09:15 +0900

    1005 로그인, 로그아웃 레이아웃구성

---
+++ src/main/java/com/jghan/SpringPJT/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.jghan.SpringPJT.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encode(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/follow/**", "/comment/**", "/api/**").authenticated() //인증이 필요한 url
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+    }
+}
--- src/main/java/com/jghan/SpringPJT/web/AuthController.java
+++ src/main/java/com/jghan/SpringPJT/web/AuthController.java
@@ -1,4 +1,32 @@
 package com.jghan.SpringPJT.web;
 
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.GeneratorType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class AuthController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/auth/signin")
+    public String signinForm(){
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm(){
+        return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(){
+        return "auth/signup";
+    }
 }
