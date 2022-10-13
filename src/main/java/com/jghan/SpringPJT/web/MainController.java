7746ea0f1a048665ad3d4228d8235a69047f040c
jghan <gojkhan13@gmail.com>
2022-10-13 14:49:32 +0900

    1013 지도 API적용

---
+++ src/main/java/com/jghan/SpringPJT/web/MainController.java
@@ -0,0 +1,21 @@
+package com.jghan.SpringPJT.web;
+
+import com.jghan.SpringPJT.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController { //추후 이름변경
+
+    @GetMapping({"/"})
+    public String story(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                        Model model){
+
+        model.addAttribute("user", principalDetails.getUser());
+
+        return "main/main";
+    }
+
+}
