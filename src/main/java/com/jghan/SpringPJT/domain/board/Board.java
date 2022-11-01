68631a4e2b96af891f6aeb0d5cc2f5b0b4108d1a
jghan <gojkhan13@gmail.com>
2022-11-01 10:39:40 +0900

    1101 게시글 검색기능 추가

--- src/main/java/com/jghan/SpringPJT/domain/board/Board.java
+++ src/main/java/com/jghan/SpringPJT/domain/board/Board.java
@@ -5,10 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 
 @Builder
@@ -28,4 +26,10 @@ public class Board {
     private String filename;
     private String filepath;
 
+    private LocalDateTime createDate;
+
+    @PrePersist //db에 INSERT되기전에 실행
+    public void createData() {
+        this.createDate = LocalDateTime.now();
+    }
 }
--- src/main/java/com/jghan/SpringPJT/domain/board/BoardRepository.java
+++ src/main/java/com/jghan/SpringPJT/domain/board/BoardRepository.java
@@ -1,6 +1,10 @@
 package com.jghan.SpringPJT.domain.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
